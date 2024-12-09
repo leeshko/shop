06bd14d3173bee1fd0a49294e21a39328a8e0760
Dmitri Lishko <dmitrilishko@gmail.com>
2024-12-09 15:20:03 -0700

    add build

---
+++ build/precache-manifest.60f3cfb998a1fa26304e6578680e5d56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b01e0017b093ef264e44",
+    "url": "/static/css/main.a7fb6454.chunk.css"
+  },
+  {
+    "revision": "b01e0017b093ef264e44",
+    "url": "/static/js/main.05eabb38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "30167f53cc60e3dc0c51",
+    "url": "/static/js/2.deecbf47.chunk.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "506afa712230d5c97c1e117d460a6808",
+    "url": "/index.html"
+  }
+];
